e1b80c179b8013c946b8faf09ac6ae81c998896b
Ravi Manchi <ravi.manchi@cba.com.au>
2015-10-14 08:08:28 +1100

    Prompting for guesses
    prompt users for a guess

--- src/au.com.testers/Hangman.java
+++ src/au.com.testers/Hangman.java
@@ -2,10 +2,23 @@ package au.com.testers;
 
 public class Hangman {
     public static void main(String[] args) {
+        //Getting Started
 //        System.out.println("Hello World!");
+
+        //Storing Guesses
         Game game = new Game("Tester");
 
-        System.out.println(game.applyGuess('t'));
-        System.out.println(game.applyGuess('x'));
+//        System.out.println(game.applyGuess('t'));
+//        System.out.println(game.applyGuess('x'));
+
+        //Prompting for guesses
+        Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+
+        if (isHit) {
+            System.out.println("We got a hit :)");
+        } else {
+            System.out.println("That was a miss :(");
+        }
     }
 }
--- src/au.com.testers/Prompter.java
+++ src/au.com.testers/Prompter.java
@@ -1,7 +1,23 @@
 package au.com.testers;
 
+import java.io.Console;
+
 /**
  * Created by ravi on 13/10/2015.
  */
 public class Prompter {
+    private Game mGame;
+
+    public Prompter(Game game) {
+        this.mGame = game;
+    }
+
+    public boolean promptForGuess() {
+        Console console = System.console();
+        String guessAsString = console.readLine("Enter a letter:  ");
+
+        char guessLetter = guessAsString.charAt(0);
+
+        return mGame.applyGuess(guessLetter);
+    }
 }
