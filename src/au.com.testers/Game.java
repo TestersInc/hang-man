ae5abcc6ecf965775ffd576d1836e49f2bfd2fb3
Ravi Manchi <ravi.manchi@cba.com.au>
2015-10-14 00:03:15 +1100

    store guesses
    Develop game

--- src/au.com.testers/Game.java
+++ src/au.com.testers/Game.java
@@ -5,8 +5,22 @@ package au.com.testers;
  */
 public class Game {
     private String mAnswer;
+    private String mHits;
+    private String mMisses;
 
     public Game(String answer) {
         this.mAnswer = answer;
+        this.mHits = "";
+        this.mMisses = "";
+    }
+
+    public boolean applyGuess(char letter) {
+        boolean isHit = mAnswer.indexOf(letter) >= 0;
+        if (isHit) {
+            mHits += letter;
+        } else {
+            mMisses += letter;
+        }
+        return isHit;
     }
 }
--- src/au.com.testers/Hangman.java
+++ src/au.com.testers/Hangman.java
@@ -4,5 +4,8 @@ public class Hangman {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
         Game game = new Game("Tester");
+
+        System.out.println(game.applyGuess('t'));
+        System.out.println(game.applyGuess('x'));
     }
 }
